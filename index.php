cd983a407d9e317867ac9759928f29e8ce22851f
EutiquioG <94626326+EutiquioG@users.noreply.github.com>
2025-06-06 21:00:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
+<?php
+include 'datos.php';
+$result = $conn->query("SELECT * FROM datos");
+//echo $result;
+?>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estación</title>
+    <title>Datos base de datos</title>
 </head>
 
 <body>
-    <h1>Hola mundokjlksjdksjlkdlksajdlkj</h1>
+    <div class="container">
+        <table class="table mt-3">
+            <tr>
+                <th>ID</th>
+                <th>temperatura</th>
+                <th>humedad</th>
+                <th>presion</th>
+                <th>fecha</th>
+            </tr>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['temperatura'] ?></td>
+                    <td><?= $row['humedad'] ?></td>
+                    <td><?= $row['presion'] ?></td>
+                    <td><?= $row['fecha'] ?></td>
+                    <td>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+    </div>
 </body>
 
 </html>
